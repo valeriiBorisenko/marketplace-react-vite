3b89ec79c6584772a62e319b1fb860faca7db09c
Valerii Borisenko <valerii.borisenko@gmail.com>
2025-03-17 09:23:31 +0100

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,13 +7,15 @@ const Header = () => {
     <header className="header">
         <div className="header__container" >
             <img className="header__logo" alt="logo" src="./img/logo-lexicon_2x.gif"/>
-            <ul className="header__links">
-                {
-                    links.map((link, index) =>
-                        <Link name={link.name} href={link.href} key={`header-link${index}`} />
-                    )
-                }
-            </ul>
+            <nav>
+                <ul className="header__links">
+                    {
+                        links.map((link, index) =>
+                            <Link name={link.name} href={link.href} key={`header-link${index}`} />
+                        )
+                    }
+                </ul>
+            </nav>
         </div>
         <h1 className="header__title">Marketplace UI!</h1>
     </header>
